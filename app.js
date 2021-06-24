035c79ce74c1fc604008e8503e1f75ce6d5845d6
ByungSoo Ye <69490984+DevelopSoo@users.noreply.github.com>
2021-06-24 21:11:45 +0900

    Feat/간단한 슬랙 봇 알림 생성 (mysql과 연동 예정)

--- app.js
+++ app.js
@@ -2,9 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-
 const lunchController = require('./src/controllers/lunchController');
 
+const Slack = require('slack-node');
+const schedule = require('node-schedule');
+const { subBusinessDays } = require('date-fns');
+
+const webhookUri = "https://hooks.slack.com/services/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"
+
+const slack = new Slack();
+slack.setWebhook(webhookUri);
+
+const send = async(message) => {
+  slack.webhook({
+    channel: "#개발",
+    username: "slack bot",
+    text: `h1 ${message}!`
+  }, function(err, response) {
+    console.log(response)
+  })
+  
+} 
+
+schedule.scheduleJob('9 * * * *', function() {
+  send("병수");
+})
 /**
  * 화면으로 보여준다고 가정할 시 사용하기
  */
@@ -26,3 +48,4 @@ app.get('/menus/:id', lunchController.lunchView);
 app.post('/menus', lunchController.lunchInput);
 app.put('/menus/:id', lunchController.lunchUpdate);
 app.delete('/menus/:id', lunchController.lunchDelete);
+
